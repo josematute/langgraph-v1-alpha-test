e1fb2e3e1dd5edc026e31fb241ef0b289644663c
Jose E Matute <60519538+josematute@users.noreply.github.com>
2025-09-26 20:08:24 -0600

    add parallelization

---
+++ parallelization.py
@@ -0,0 +1,75 @@
+from typing_extensions import TypedDict
+from langgraph.graph import StateGraph, START, END
+from llm_config import llm, quick_save_diagram
+
+
+# Graph state
+class State(TypedDict):
+    topic: str
+    joke: str
+    story: str
+    poem: str
+    combined_output: str
+
+
+# Nodes
+def call_llm_1(state: State):
+    """First LLM call to generate initial joke"""
+
+    print("call_llm_1 running...")
+    msg = llm.invoke(f"Write a joke about {state['topic']}")
+    return {"joke": msg.content}
+
+
+def call_llm_2(state: State):
+    """Second LLM call to generate story"""
+
+    print("call_llm_2 running...")
+    msg = llm.invoke(f"Write a story about {state['topic']}")
+    return {"story": msg.content}
+
+
+def call_llm_3(state: State):
+    """Third LLM call to generate poem"""
+
+    print("call_llm_3 running...")
+    msg = llm.invoke(f"Write a poem about {state['topic']}")
+    return {"poem": msg.content}
+
+
+def aggregator(state: State):
+    """Combine the joke and story into a single output"""
+
+    print("aggregator running...") 
+    combined = f"Here's a story, joke, and poem about {state['topic']}!\n\n"
+    combined += f"STORY:\n{state['story']}\n\n"
+    combined += f"JOKE:\n{state['joke']}\n\n"
+    combined += f"POEM:\n{state['poem']}"
+    return {"combined_output": combined}
+
+
+# Build workflow
+parallel_builder = StateGraph(State)
+
+# Add nodes
+parallel_builder.add_node("call_llm_1", call_llm_1)
+parallel_builder.add_node("call_llm_2", call_llm_2)
+parallel_builder.add_node("call_llm_3", call_llm_3)
+parallel_builder.add_node("aggregator", aggregator)
+
+# Add edges to connect nodes
+parallel_builder.add_edge(START, "call_llm_1")
+parallel_builder.add_edge(START, "call_llm_2")
+parallel_builder.add_edge(START, "call_llm_3")
+parallel_builder.add_edge("call_llm_1", "aggregator")
+parallel_builder.add_edge("call_llm_2", "aggregator")
+parallel_builder.add_edge("call_llm_3", "aggregator")
+parallel_builder.add_edge("aggregator", END)
+parallel_workflow = parallel_builder.compile()
+
+# Show workflow
+quick_save_diagram(parallel_workflow, "my_workflow")
+
+# Invoke
+state = parallel_workflow.invoke({"topic": "cats"})
+print(state["combined_output"])
